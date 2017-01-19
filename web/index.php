75a37635ed73f7e7c4a74558673b4b9f2bde3234
unknown <C:\Users\USER\AppData\Roaming\The Bat!>
2017-01-19 15:01:26 +0300

    реализуем index.php

--- web/index.php
+++ web/index.php
@@ -1,1 +1,7 @@
-Our CRM
+<?php
+//Including the Yii framework itself
+require(__DIR__. '/../vendor/yiisoft/yii2/Yii.php');
+//Getting the configuration
+$config = require(__DIR__. '/../config/web.php');
+//Making and launching the application immediatly
+(new yii\web\Application($config))->run();
