591551978be7dd4a1eb76be32e71fcf4d9d8db8e
unknown <C:\Users\USER\AppData\Roaming\The Bat!>
2017-01-25 17:25:29 +0300

    исправил

--- views/customers/_customer.php
+++ views/customers/_customer.php
@@ -3,9 +3,10 @@ echo \yii\widgets\DetailView::widget(
     [
         'model' => $model,
         'attributes' =>[
-            ['attribute' => 'birth_date', 'value' => $model->birth_date->format('Y-m-d'),
+            ['attribute' => 'name'],
+            ['attribute' => 'birth_date', 'value' => $model->birth_date->format('Y-m-d')],
             'notes:text',
             ['label' => 'Phone Number', 'attribute' => 'phones.0.number']]
-        ]
+
     ]
 );
