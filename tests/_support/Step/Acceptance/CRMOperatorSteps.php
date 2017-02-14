29ffcf452de09e174d0b670e428057e10976d514
cnx <cnx08@mail.ru>
2017-02-14 16:31:09 +0400

    add wait on submit

--- tests/_support/Step/Acceptance/CRMOperatorSteps.php
+++ tests/_support/Step/Acceptance/CRMOperatorSteps.php
@@ -31,6 +31,7 @@ class CRMOperatorSteps extends \AcceptanceTester
     {
         $I = $this;
         $I->click('Submit');
+        $I->wait(1);
     }
 
     public function seeIAmInListCustomersUi()
